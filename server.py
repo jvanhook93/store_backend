da9d348f24ae374c0ffa0ca02b9e90460ff0c957
Hadria <javanhook93@gmail.com>
2022-08-30 21:57:24 -0500

    Final

--- server.py
+++ server.py
@@ -3,10 +3,11 @@ from flask import Flask, request, abort
 import json
 import random
 from data import me, catalog
+from flask_cors import CORS
 
 
 app = Flask(__name__)
-
+CORS(app)
 
 
 @app.get("/")
